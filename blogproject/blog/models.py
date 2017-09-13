37f82d7b933eaf94e6dbb0ef3caddbbc127e3607
nanfengpo <lsxxxxxx@126.com>
2017-09-13 11:28:42 +0800

    Initial commit

---
+++ blogproject/blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    def __str__(self):
+        return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=64)
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=64)
+    body  = models.TextField()
+    created_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    digest = models.CharField(max_length=256,blank=True)
+
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag,blank=True)
+
+    author = models.ForeignKey(User)
+
+    def get_absolute_url(self):
+        return reverse('blog:detail',kwargs={'pk':self.pk})
+
+
+    def __str__(self):
+        return self.title
---
+++ blogproject/blog/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse
+# Create your views here.
+from .models import Post,Category
+from markdown import markdown
+from comment.forms import CommentForm
+
+def index(request):
+    post_list = Post.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'post_list':post_list})
+    # return HttpResponse('<h1>hellow world</h1>')
+
+
+def detail(request,pk):
+    post = get_object_or_404(Post,pk = pk)
+    post.body = markdown(post.body,
+                         extensions=[
+                             'markdown.extensions.extra',
+                             'markdown.extensions.codehilite',
+                             'markdown.extensions.toc',
+                         ])
+    form = CommentForm()
+    comment_list = post.comment_set.all()
+    context = {
+        'post': post,
+        'form': form,
+        'comment_list': comment_list,
+    }
+    return render(request,'blog/detail.html',context=context)
+
+
+def archives(request, year, month):
+    post_list = Post.objects.filter(created_time__year=year,
+                                    created_time__month=month).order_by('-created_time')
+    return render(request,'blog/index.html',context={'post_list': post_list})
+
+def category(request,pk):
+    cate = get_object_or_404(Category,pk = pk)
+    post_list = Post.objects.filter(category=cate).order_by('-created_time')
+    return render(request,'blog/index.html',context={'post_list': post_list})
